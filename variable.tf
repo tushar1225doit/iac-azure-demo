9789107d80d13b807bd7ce278267de82c43bbd87
Your Name <tushar1225.doit@gmail.com>
2025-09-03 17:07:20 +0200

    fixing public key error by putting key in secretsv5

--- variable.tf
+++ variable.tf
@@ -82,3 +82,16 @@ variable "ssh_public_key" {
   description = "The SSH public key for the VM"
   type        = string
 }
+variable "subscription_id" {
+  type = string
+}
+variable "client_id" {
+  type = string
+}
+variable "client_secret" {
+  type      = string
+  sensitive = true
+}
+variable "tenant_id" {
+  type = string
+}
